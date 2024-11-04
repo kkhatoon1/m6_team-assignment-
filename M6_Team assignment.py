40f515715bd492d5d8ab38a80b6f62c9e0144bcd
kkhatoon1 <kkhatoon1@my.nl.edu>
2024-11-03 21:04:06 -0600

    Update M6_Team assignment.py

--- M6_Team assignment.py
+++ M6_Team assignment.py
@@ -8,8 +8,10 @@ Anw = int(input("Please guess a number between 1 and 10: "))
 random_number = random.randrange(1,10)
 
 while Anw > random_number:
+    if Anw < 1 or Anw > 10:break 
     print(f"Please guess lower")
     Anw = int(input("Please guess a number between 1 and 10: "))
+    if Anw < 1 or Anw > 10:break 
 while Anw < random_number:
     print(f"Please guess higher")
     Anw = int(input("Please guess a number between 1 and 10: "))
